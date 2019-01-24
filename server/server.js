b8fbf95054acd69cc6c41a44365819cf95c5a01a
Pescia Ivo <ivo.pescia@appway.com>
2019-01-24 12:01:51 +0100

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -50,6 +50,32 @@ app.get('/todos/:id', (req, res) => {
     //res.send(req.params);
 });
 
+app.delete('/todos/:id', (req, res) => {
+    // get the id
+    var id = req.params.id;
+
+    // validate the id -> not valid? return 404
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+    // remove todo by id
+        // success
+            // if no doc, send 404
+            // if doc, send doc back with 200
+        // error
+            // send 400 with empty body
+
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
