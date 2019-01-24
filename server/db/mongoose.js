bad977827e8c7b6c1981339436a40c5199ce9cf3
Pescia Ivo <ivo.pescia@appway.com>
2019-01-24 11:29:51 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect( 'mongodb://udemyuser:appway19@ds111425.mlab.com:11425/nodecrs' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
