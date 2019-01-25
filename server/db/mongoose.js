acb1d74d40b889250057cb280da0bd9a1bd7ffa0
Pescia Ivo <ivo.pescia@appway.com>
2019-01-25 13:18:52 +0100

    setup separate test and development environment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://udemyuser:appway19@ds111425.mlab.com:11425/nodecrs' || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect( 'mongodb://udemyuser:appway19@ds111425.mlab.com:11425/nodecrs' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
