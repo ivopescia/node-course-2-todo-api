ab8364b716a66d8f8b4f3c7a57f50289cdf42dd4
Pescia Ivo <ivo.pescia@appway.com>
2019-01-25 16:21:29 +0100

    added authentication private route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+var {authenticate} = require('./middleware/authenticate.js');
 
 var app = express();
 const port = process.env.PORT;
@@ -118,6 +119,11 @@ app.post('/users', (req, res) => {
     }).catch ((e) => {
         res.status(400).send(e);
     })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
